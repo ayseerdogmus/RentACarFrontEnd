b43f3d3e48fcecb8604164a2f8c6addd5c64e203
ayseebiclioglu <ayseebiclioglu@gmail.com>
2021-04-05 00:44:37 +0300

    RentACar

---
+++ src/app/models/rental.ts
@@ -0,0 +1,12 @@
+
+export interface Rental  {
+        rentalId :number;
+        FirstName :string;
+        LastName :string;
+        Email :string;
+        CompanyName:string; 
+        RentDate :Date;
+        ReturnDate :Date;
+        ModelYear :number;
+        BrandName :string
+}
---
+++ src/app/models/userResponseModel.ts
@@ -0,0 +1,7 @@
+import { ResponseModel } from "./responseModel";
+import { User } from "./user";
+
+export interface UserResponseModel extends ResponseModel{
+    data:User[],
+   
+}
